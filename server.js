78f21996f76a8116b5cafebef1190f0fe0de1766
gettingold@mail.ru <bit.size.dev@gmail.com>
2018-09-08 20:12:58 +0300

    Angular app

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const PORT = process.env.PORT || 5000;
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const config = require('./server/config');
+
+const apiRoutes = require('./server/routes/v1');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+
+// Routes
+app.use('/api', apiRoutes);
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+app.set('port', PORT);
+
+const server = http.createServer(app);
+server.listen(PORT, () => {
+    console.log('Server running.');
+});
